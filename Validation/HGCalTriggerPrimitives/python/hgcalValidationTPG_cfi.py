b2c06cbf4bb1daec70b1a7ffb8b9e78509ac20e7
Emilia Becheva <emilia.becheva@llr.in2p3.fr>
2018-10-23 16:43:29 +0200

    Change the name of the main class

--- Validation/HGCalTriggerPrimitives/python/hgcalValidationTPG_cfi.py
+++ Validation/HGCalTriggerPrimitives/python/hgcalValidationTPG_cfi.py
@@ -3,7 +3,7 @@ from DQMServices.Core.DQMEDAnalyzer import DQMEDAnalyzer
 from L1Trigger.L1THGCal.egammaIdentification import egamma_identification_drnn_cone
 
 hgcalTrigPrimValidation = DQMEDAnalyzer(
-        "HGCalTrigValidator",
+        "HGCalTriggerValidator",
         TriggerCells = cms.InputTag('hgcalConcentratorProducer:HGCalConcentratorProcessorSelection'),
         Clusters = cms.InputTag('hgcalBackEndLayer1Producer:HGCalBackendLayer1Processor2DClustering'),
         Multiclusters = cms.InputTag('hgcalBackEndLayer2Producer:HGCalBackendLayer2Processor3DClustering'),
